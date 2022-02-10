c132c0e221ba0818cf84eab1dbd90378fb549983
zhanzeyi <zhanzeyi@huawei.com>
2022-02-10 19:48:50 +0800

    update autosign script
    Signed-off-by: zhanzeyi <zhanzeyi@huawei.com>

--- autosign/create_appcert_sign_profile.sh
+++ autosign/create_appcert_sign_profile.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-python3 autosign.py generate
+python3 autosign.py createAppCertAndProfile
