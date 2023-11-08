033be23a79a63dd49b4ccecd0b9b68418a573d6e
zhanzeyi <zhanzeyi@huawei.com>
2023-11-08 18:04:00 +0800

    change elf profile type
    Signed-off-by: zhanzeyi <zhanzeyi@huawei.com>

--- hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/hap/provider/SignProvider.java
+++ hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/hap/provider/SignProvider.java
@@ -432,6 +432,9 @@ public abstract class SignProvider {
         publicCerts = getPublicCerts();
         // 3. load optionalBlocks
         loadOptionalBlocks();
+        if ("elf".equals(options.getString(ParamConstants.PARAM_IN_FORM))) {
+            return publicCerts;
+        }
         checkProfileValid(publicCerts);
         return publicCerts;
     }
