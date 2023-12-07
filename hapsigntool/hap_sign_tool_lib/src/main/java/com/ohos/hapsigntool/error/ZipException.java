42b8d66ccbfb51ade064d9d88b7d64c6caa0434c
wangzeyu <wangzeyu38@huawei.com>
2023-12-07 02:19:46 +0000

    codecheck repair
    Signed-off-by: wangzeyu <wangzeyu38@huawei.com>

--- hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/error/ZipException.java
+++ hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/error/ZipException.java
@@ -2,15 +2,27 @@ package com.ohos.hapsigntool.error;
 
 import java.io.IOException;
 
+/**
+ * Zip exception for programs.
+ *
+ * @since 2023/12/07
+ */
 public class ZipException extends IOException {
-    public ZipException() {
-
-    }
-
+    /**
+     * new ZipException
+     * 
+     * @param message exception message
+     */
     public ZipException(String message) {
         super(message);
     }
 
+    /**
+     * new ZipException
+     * 
+     * @param message exception message
+     * @param e exception
+     */
     public ZipException(String message, Exception e) {
         super(message, e);
     }
