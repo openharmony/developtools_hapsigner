f1fa46b70c22777a45a112b4941bee4efca46e28
zhanzeyi <zhanzeyi@huawei.com>
2024-07-25 20:20:36 +0800

    update
    Signed-off-by: zhanzeyi <zhanzeyi@huawei.com>

--- hapsigntool_cpp/api/src/sign_tool_service_impl.cpp
+++ hapsigntool_cpp/api/src/sign_tool_service_impl.cpp
@@ -332,7 +332,7 @@ bool SignToolServiceImpl::OutputString(std::string content, std::string file)
 bool SignToolServiceImpl::X509CertVerify(X509* cert, EVP_PKEY* privateKey)
 {
     if (!X509_verify(cert, privateKey)) {
-        PrintErrorNumberMsg("VERIFY_ERROR", VERIFY_ERROR, "private key verify cert failed!");
+        PrintErrorNumberMsg("VERIFY_ERROR", VERIFY_ERROR, "verify cert failed!");
         return false;
     }
     return true;
@@ -688,7 +688,7 @@ bool SignToolServiceImpl::VerifyHapSigner(Options* option)
         PrintMsg("bin verify successed!");
         return true;
     } else {
-        PrintErrorNumberMsg("NOT_SUPPORT_ERROR", NOT_SUPPORT_ERROR, "This requirement was not implemented !");
+        PrintErrorNumberMsg("NOT_SUPPORT_ERROR", NOT_SUPPORT_ERROR, "Unsupported inForm!");
         return false;
     }
 }
