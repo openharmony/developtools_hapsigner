a9c5f0e94badf540f5e8547dad428a82864c7ea3
wangzeyu <wangzeyu38@huawei.com>
2024-09-04 16:55:12 +0800

    revert cpp and bitmap
    Signed-off-by: wangzeyu <wangzeyu38@huawei.com>

--- hapsigntool/hap_sign_tool/src/test/java/com/ohos/hapsigntoolcmd/CmdUnitTest.java
+++ hapsigntool/hap_sign_tool/src/test/java/com/ohos/hapsigntoolcmd/CmdUnitTest.java
@@ -20,9 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.ohos.hapsigntool.HapSignTool;
-import com.ohos.hapsigntool.codesigning.utils.HapUtils;
 import com.ohos.hapsigntool.error.CustomException;
-import com.ohos.hapsigntool.error.ProfileException;
 import com.ohos.hapsigntool.utils.KeyPairTools;
 import com.ohos.hapsigntool.utils.FileUtils;
 import com.ohos.hapsigntool.zip.Zip;
@@ -47,9 +45,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.jar.JarFile;
 import java.util.zip.CRC32;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -840,13 +836,6 @@ public class CmdUnitTest {
         multiBundleTest(".hqf");
     }
 
-    @Order(13)
-    @Test
-    public void testCmdMultiHnp() throws IOException {
-        loadFile(CMD_TEST_HAP_FILE_PATH);
-        File unsignedHap = new File(CMD_TEST_HAP_FILE_PATH);
-        signAndVerifyHap(unsignedHap.getAbsolutePath(), ".hap");
-    }
 
     private void multiBundleTest(String bundleSuffix) throws IOException {
         for (FileType abcFile : FileType.values()) {
@@ -1002,20 +991,6 @@ public class CmdUnitTest {
         incorrectName.add("中文.so.111.111.json");
         for (String name : incorrectName) {
             assertFalse(FileUtils.isRunnableFile(name));
-        }
-    }
-
-    @Test
-    public void testGetHnpsFromHap() throws IOException, ProfileException {
-        loadFile(CMD_TEST_HAP_FILE_PATH);
-        File file = new File(CMD_TEST_HAP_FILE_PATH);
-        try (JarFile inputJar = new JarFile(file, false)) {
-            Map<String, String> hnpMap = HapUtils.getHnpsFromJson(inputJar);
-            assertTrue(hnpMap.size() == 4);
-            assertTrue("private".equals(hnpMap.get("entry-default-unsigned.hnp")));
-            assertTrue("public".equals(hnpMap.get("entry-default-unsigned1.hnp")));
-            assertTrue("private".equals(hnpMap.get("dd/entry-default-unsigned.hnp")));
-            assertTrue("public".equals(hnpMap.get("dd/entry-default-unsigned1.hnp")));
         }
     }
 
