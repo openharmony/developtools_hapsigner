13d23f3d0d776fada12a22f5e02ab5802718d1ab
zfeixiang <zhangfeixiang9@huawei.com>
2025-01-08 14:35:17 +0800

    error code for ide
    Signed-off-by: zfeixiang <zhangfeixiang9@huawei.com>

--- hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/codesigning/utils/HapUtils.java
+++ hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/codesigning/utils/HapUtils.java
@@ -28,18 +28,14 @@ import com.ohos.hapsigntool.entity.Pair;
 import com.ohos.hapsigntool.error.ProfileException;
 import com.ohos.hapsigntool.utils.LogUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
