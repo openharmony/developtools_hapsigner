d91fce834cde13808bbe41d7bdac0dbc2bae5380
zfeixiang <zhangfeixiang9@huawei.com>
2025-01-11 17:18:13 +0800

    error code for ide
    Signed-off-by: zfeixiang <zhangfeixiang9@huawei.com>

--- hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/codesigning/exception/CodeSignErrMsg.java
+++ hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/codesigning/exception/CodeSignErrMsg.java
@@ -27,7 +27,7 @@ public class CodeSignErrMsg {
      * FILE_FORMAT_UNSUPPORTED_ERROR
      */
     public static final ErrorMsg FILE_FORMAT_UNSUPPORTED_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("10")
+        .addTypeCode("06")
         .addErrCode("001")
         .addDescription("File Format Error")
         .addCause("Code sign does not support the file format")
@@ -35,32 +35,11 @@ public class CodeSignErrMsg {
         .build();
 
     /**
-     * EXTRACT_HNP_FILE_ERROR
-     */
-    public static final ErrorMsg EXTRACT_HNP_FILE_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("10")
-        .addErrCode("002")
-        .addDescription("File Format Error")
-        .addCause("Extract hnp file {%s} error")
-        .addSolution("Check whether the hnp file is packaged correctly")
-        .build();
-
-    /**
-     * FILE_4K_ALIGNMENT_ERROR
-     */
-    public static final ErrorMsg FILE_4K_ALIGNMENT_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("10")
-        .addErrCode("003")
-        .addDescription("File Alignment Error")
-        .addCause("Invalid data size {%d}, not a multiple of 4096")
-        .build();
-
-    /**
      * READ_INPUT_STREAM_ERROR
      */
     public static final ErrorMsg READ_INPUT_STREAM_ERROR = ErrorMsg.getCodeSignErrBuilder()
         .addTypeCode("10")
-        .addErrCode("004")
+        .addErrCode("001")
         .addDescription("Input Stream Read Error")
         .addCause("Read buffer from input error")
         .build();
@@ -74,6 +53,17 @@ public class CodeSignErrMsg {
         .addDescription("Certificates Error")
         .addCause("%s")
         .addSolution("Please check whether the certificate is correct")
+        .build();
+
+    /**
+     * SIGNATURE_VERIFY_FAILED_ERROR
+     */
+    public static final ErrorMsg SIGNATURE_VERIFY_FAILED_ERROR = ErrorMsg.getCodeSignErrBuilder()
+        .addTypeCode("11")
+        .addErrCode("002")
+        .addDescription("Certificates Error")
+        .addCause("Signature failed")
+        .addSolution("Please check whether the keyAlias is correct")
         .build();
 
     /**
@@ -146,8 +136,8 @@ public class CodeSignErrMsg {
      * MODULE_JSON_PARSE_ERROR
      */
     public static final ErrorMsg MODULE_JSON_PARSE_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("13")
-        .addErrCode("001")
+        .addTypeCode("12")
+        .addErrCode("002")
         .addDescription("Module Content Error")
         .addCause("Module json content is invalid")
         .addSolution("Please check whether the module json is correct")
@@ -157,7 +147,7 @@ public class CodeSignErrMsg {
      * HNP_FILE_DESCRIPTION_ERROR
      */
     public static final ErrorMsg HNP_FILE_DESCRIPTION_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("13")
+        .addTypeCode("12")
         .addErrCode("002")
         .addDescription("Module Content Error")
         .addCause("Hnp {%s} is not described in module.json")
@@ -165,55 +155,65 @@ public class CodeSignErrMsg {
         .build();
 
     /**
-     * DIGEST_ALGORITHM_ERROR
-     */
-    public static final ErrorMsg DIGEST_ALGORITHM_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("14")
-        .addErrCode("001")
-        .addDescription("Digest Algorithm Error")
-        .addCause("Invalid algorithm {%s}")
-        .addSolution("Perhaps the JDK you are using does not support it")
-        .build();
-
-    /**
-     * CODE_SIGN_INTERNAL_ERROR
-     */
-    public static final ErrorMsg CODE_SIGN_INTERNAL_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("15")
-        .addErrCode("001")
-        .addDescription("Code Sign Internal Error")
-        .addCause("%s")
-        .build();
-
-    /**
-     * SIGNATURE_VERIFY_FAILED_ERROR
-     */
-    public static final ErrorMsg SIGNATURE_VERIFY_FAILED_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("15")
-        .addErrCode("002")
-        .addDescription("Code Sign Error")
-        .addCause("Signature verify failed")
-        .addSolution("Please check whether the keyAlias is correct")
-        .build();
-
-    /**
      * ELF_FILE_HEADER_ERROR
      */
     public static final ErrorMsg ELF_FILE_HEADER_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("16")
-        .addErrCode("001")
+        .addTypeCode("12")
+        .addErrCode("003")
         .addDescription("Elf File Error")
         .addCause("ELF {%s} is incorrect")
         .addSolution("Failed to read the elf file, please check whether the file header information is correct")
         .build();
 
     /**
+     * EXTRACT_HNP_FILE_ERROR
+     */
+    public static final ErrorMsg EXTRACT_HNP_FILE_ERROR = ErrorMsg.getCodeSignErrBuilder()
+        .addTypeCode("12")
+        .addErrCode("004")
+        .addDescription("File Format Error")
+        .addCause("Extract hnp file {%s} error")
+        .addSolution("Check whether the hnp file is packaged correctly")
+        .build();
+
+    /**
+     * DIGEST_ALGORITHM_ERROR
+     */
+    public static final ErrorMsg ALGORITHM_NOT_SUPPORT_ERROR = ErrorMsg.getCodeSignErrBuilder()
+        .addTypeCode("13")
+        .addErrCode("001")
+        .addDescription("Digest Algorithm Error")
+        .addCause("Invalid algorithm {%s}")
+        .addSolution("Using SHA-256 algorithm, if an error occurs, please check the JDK version you are using")
+        .build();
+
+    /**
+     * CODE_SIGN_INTERNAL_ERROR
+     */
+    public static final ErrorMsg CODE_SIGN_INTERNAL_ERROR = ErrorMsg.getCodeSignErrBuilder()
+        .addTypeCode("14")
+        .addErrCode("001")
+        .addDescription("Code Sign Internal Error")
+        .addCause("%s")
+        .build();
+
+    /**
+     * FILE_4K_ALIGNMENT_ERROR
+     */
+    public static final ErrorMsg FILE_4K_ALIGNMENT_ERROR = ErrorMsg.getCodeSignErrBuilder()
+        .addTypeCode("14")
+        .addErrCode("001")
+        .addDescription("Code Sign Internal Error")
+        .addCause("Invalid data size {%d}, not a multiple of 4096")
+        .build();
+
+    /**
      * PAGE_INFO_UNIT_SIZE_ERROR
      */
     public static final ErrorMsg PAGE_INFO_ERROR = ErrorMsg.getCodeSignErrBuilder()
-        .addTypeCode("17")
+        .addTypeCode("14")
         .addErrCode("001")
-        .addDescription("Page Info Error")
-        .addCause("%s")
+        .addDescription("Code Sign Internal Error")
+        .addCause("Page Info Error : %s")
         .build();
 }
