2f27b1111209900d2886f52f6e550785458a319f
zfeixiang <zhangfeixiang9@h-partners.com>
2025-07-03 16:32:13 +0800

    verify elf
    Signed-off-by: zfeixiang <zhangfeixiang9@h-partners.com>

--- binary_sign_tool/hap/verify/src/verify_elf.cpp
+++ binary_sign_tool/hap/verify/src/verify_elf.cpp
@@ -19,17 +19,12 @@
 #include "constant.h"
 #include "file_utils.h"
 #include "signature_tools_log.h"
+#include "verify_hap_openssl_utils.h"
 #include "hash_utils.h"
-#include "pkcs7_data.h"
 
 namespace OHOS {
 namespace SignatureTools {
 
-const int8_t VerifyElf::SIGNATURE_BLOCK = 0;
-const int8_t VerifyElf::PROFILE_NOSIGNED_BLOCK = 1;
-const int8_t VerifyElf::PROFILE_SIGNED_BLOCK = 2;
-const int8_t VerifyElf::KEY_ROTATION_BLOCK = 3;
-const int8_t VerifyElf::CODESIGNING_BLOCK_TYPE = 3;
 const std::string VerifyElf::codesignSec = ".codesign";
 const std::string VerifyElf::profileSec = ".profile";
 const std::string VerifyElf::permissionSec = ".permission";
@@ -50,14 +45,7 @@ bool VerifyElf::Verify(Options* options)
 
 
     // get codesignSec section
-
     ParseSignBlock(elfReader);
-
-    // get profileSec section
-
-
-
-    // get permissionSec section
 
     return true;
 }
@@ -83,6 +71,16 @@ bool VerifyElf::ParseSignBlock(const ELFIO::elfio& elfReader)
     }
     const ElfSignInfo* signInfo = reinterpret_cast<const ElfSignInfo*>(data);
     PrintMsg("codesign section offset: " + std::to_string(signInfo->dataSize));
+
+    Pkcs7Context pkcs7Context;
+
+    VerifyAppPkcs7(pkcs7Context, reinterpret_cast<const unsigned char*>(signInfo->signature), signInfo->signSize);
+
+        if (!PrintCertChainToCmd(pkcs7Context.certChain[0])) {
+            SIGNATURE_TOOLS_LOGE("print cert chain to cmd failed\n");
+            return false;
+        }
+
     return true;
 }
 
@@ -104,6 +102,50 @@ bool VerifyElf::GetRawContent(const std::vector<int8_t>& contentVec, std::string
         SIGNATURE_TOOLS_LOGE("get p7Data raw content failed!");
         return false;
     }
+    return true;
+}
+
+bool VerifyElf::VerifyAppPkcs7(Pkcs7Context& pkcs7Context, const unsigned char* pkcs7Block, uint32_t pkcs7Len)
+{
+    // const unsigned char* pkcs7Block = reinterpret_cast<const unsigned char*>(hapSignatureBlock.GetBufferPtr());
+    // uint32_t pkcs7Len = static_cast<unsigned int>(hapSignatureBlock.GetCapacity());
+    if (!VerifyHapOpensslUtils::ParsePkcs7Package(pkcs7Block, pkcs7Len, pkcs7Context)) {
+        SIGNATURE_TOOLS_LOGE("parse pkcs7 failed");
+        return false;
+    }
+    if (!VerifyHapOpensslUtils::GetCertChains(pkcs7Context.p7, pkcs7Context)) {
+        SIGNATURE_TOOLS_LOGE("GetCertChains from pkcs7 failed");
+        return false;
+    }
+    if (!VerifyHapOpensslUtils::VerifyPkcs7(pkcs7Context)) {
+        SIGNATURE_TOOLS_LOGE("verify signature failed");
+        return false;
+    }
+    return true;
+}
+
+bool VerifyElf::PrintCertChainToCmd(std::vector<X509*>& certChain)
+{
+    BIO* outFd = BIO_new_fp(stdout, BIO_NOCLOSE);
+    if (!outFd) {
+        PrintErrorNumberMsg("IO_ERROR", IO_ERROR, "The stdout stream may have errors");
+        return false;
+    }
+    uint64_t format = XN_FLAG_SEP_COMMA_PLUS; // Print according to RFC2253
+    uint64_t content = X509_FLAG_NO_EXTENSIONS | X509_FLAG_NO_ATTRIBUTES | X509_FLAG_NO_HEADER | X509_FLAG_NO_SIGDUMP;
+    int num = 0;
+    for (auto& cert : certChain) {
+        PrintMsg("+++++++++++++++++++++++++++++++++certificate #" + std::to_string(num) +
+                 "+++++++++++++++++++++++++++++++++++++");
+        if (!X509_print_ex(outFd, cert, format, content)) {
+            VerifyHapOpensslUtils::GetOpensslErrorMessage();
+            SIGNATURE_TOOLS_LOGE("print x509 cert to cmd failed");
+            BIO_free(outFd);
+            return false;
+        }
+        ++num;
+    }
+    BIO_free(outFd);
     return true;
 }
 
