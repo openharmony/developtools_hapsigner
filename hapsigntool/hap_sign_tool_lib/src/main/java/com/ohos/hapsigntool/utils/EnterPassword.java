e04273b1630ea581b89c8ac57e0761bff5fee7a9
sunzibin001 <sunzibin1@h-partners.com>
2025-12-22 14:59:54 +0800

    comment
    Signed-off-by: sunzibin001 <sunzibin1@h-partners.com>

--- hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/utils/EnterPassword.java
+++ hapsigntool/hap_sign_tool_lib/src/main/java/com/ohos/hapsigntool/utils/EnterPassword.java
@@ -17,11 +17,10 @@ package com.ohos.hapsigntool.utils;
 
 import com.ohos.hapsigntool.entity.Options;
 import com.ohos.hapsigntool.error.CustomException;
-import com.ohos.hapsigntool.error.ERROR;
 import java.io.Console;
 import java.io.File;
-import java.security.UnrecoverableKeyException;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Scanner;
 
 /**
  * Provider function of enter password.
@@ -33,10 +32,11 @@ public class EnterPassword {
      * Add log info.
      */
     private static final LogUtils LOGGER = new LogUtils(EnterPassword.class);
-    private static final String PROMPT_KEY_PWD = "please input KeyPwd:";
-    private static final String PROMPT_KEYSTORE_PWD = "please input KeystorePwd:";
-    private static final String PROMPT_ISSUER_KEY_PWD = "please input IssuerKeyPwd:";
-    private static final String PROMPT_ISSUER_KEYSTORE_PWD = "please input IssuerKeystorePwd:";
+    private static final String PROMPT_KEY_PWD = "please input KeyPwd (timeout 30 seconds):";
+    private static final String PROMPT_KEYSTORE_PWD = "please input KeystorePwd (timeout 30 seconds):";
+    private static final String PROMPT_ISSUER_KEY_PWD = "please input IssuerKeyPwd (timeout 30 seconds):";
+    private static final String PROMPT_ISSUER_KEYSTORE_PWD = "please input IssuerKeystorePwd (timeout 30 seconds):";
+    private static final int MAX_WAIT_TIME = 30000;
 
     /**
      * Constructor of Method
@@ -44,6 +44,52 @@ public class EnterPassword {
     private EnterPassword() {
     }
 
+    /**
+     * Reads a password with a specified timeout. If no input is received within the timeout, the input is closed.
+     * 
+     * @param promptMessage The prompt message to display to the user.
+     * @return The password entered by the user, or null if not input is received.
+     */
+    private static char[] readPasswordWithTimeout(String promptMessage) {
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("Console not available. Please run in a terminal environment.");
+            return null;
+        }
+
+        final char[][] password = {null};
+        Scanner scanner = new Scanner(System.in);
+        Thread readPasswordThread = new Thread(() -> {
+            System.out.print(promptMessage);
+            password[0] = scanner.nextLine().toCharArray();
+        });
+
+        readPasswordThread.start();
+
+        try {
+            readPasswordThread.join(MAX_WAIT_TIME);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        if (password[0] == null) {
+            readPasswordThread.interrupt();
+            System.out.println();
+            LOGGER.error("No password input, closing input.");
+            console.flush();
+        }
+
+        return password[0];
+    }
+
+    /**
+     * Updates the password for a key alias.
+     * 
+     * @param params The options object containing various parameters.
+     * @param keyStoreEntry A SimpleEntry containing the key for the keystore file and its password.
+     * @param keyAliasEntry A SimpleEntry containing the key for the key alias and its rights.
+     * @param promptMessage The prompt message to display to the user.
+     */
     private static void updateKeyAliasPassword(
             Options params,
             SimpleEntry<String, String> keyStoreEntry,
@@ -76,15 +122,23 @@ public class EnterPassword {
             KeyStoreHelper tempKeyStoreHelper = new KeyStoreHelper(keyStoreFile, (char[]) keyStorePwd);
             tempKeyStoreHelper.loadPrivateKey(alias, new char[0]);
         } catch (CustomException exception) {
-            if (exception.getMessage().contains("KEY_PASSWORD_ERROR")) {
+            if (exception.getMessage().contains("password error")) {
                 LOGGER.error("Failed to load PrivateKey with empty password. open console input keyPwd");
-                Console console = System.console();
-                char[] password = console.readPassword(promptMessage);
+                char[] password = readPasswordWithTimeout(promptMessage);
                 params.put(keyRightsKey, password);
             }
         }
     }
 
+    /**
+     * Updates the keystore password.
+     * 
+     * @param params The options object containing various parameters.
+     * @param keyStoreFileKey The key for the keystore file path.
+     * @param keyStorePwdKey The key for the keystore password.
+     * @param keyAliasKey The key for the key alias.
+     * @param promptMessage The prompt message to display to the user.
+     */
     private static void updateKeystorePassword(
             Options params,
             String keyStoreFileKey,
@@ -111,33 +165,58 @@ public class EnterPassword {
         try {
             new KeyStoreHelper(keyStoreFile, new char[0]);
         } catch (CustomException exception) {
-            if (exception.getMessage().contains("INIT_KEYSTORE_FAILED")) {
+            if (exception.getMessage().contains("Init keystore failed")) {
                 LOGGER.error("Failed to load Keystore with empty password. open console input pwd");
-                Console console = System.console();
-                char[] password = console.readPassword(promptMessage);
+                char[] password = readPasswordWithTimeout(promptMessage);
                 params.put(keyStorePwdKey, password);
             }
         }
     }
 
+    /**
+     * Updates the password parameters for key alias.
+     * 
+     * @param params The options object containing various parameters.
+     */
     public static void updateParamForKeyPwd(Options params) {
-        SimpleEntry<String, String> keyStoreEntry = new SimpleEntry<>(Options.KEY_STORE_FILE, Options.KEY_STORE_RIGHTS);
-        SimpleEntry<String, String> keyAliasEntry = new SimpleEntry<>(Options.KEY_ALIAS, Options.KEY_RIGHTS);
+        SimpleEntry<String, String> keyStoreEntry = new SimpleEntry<>(Options.KEY_STORE_FILE,
+                                                                      Options.KEY_STORE_RIGHTS);
+        SimpleEntry<String, String> keyAliasEntry = new SimpleEntry<>(Options.KEY_ALIAS,
+                                                                      Options.KEY_RIGHTS);
         updateKeyAliasPassword(params, keyStoreEntry, keyAliasEntry, PROMPT_KEY_PWD);
     }
 
+    /**
+     * Updates the password parameters for issuer key alias from keystore.
+     * 
+     * @param params The options object containing various parameters.
+     */
     public static void updateParamForIssuerKeyPwdFromKeystore(Options params) {
-        SimpleEntry<String, String> keyStoreEntry = new SimpleEntry<>(Options.KEY_STORE_FILE, Options.KEY_STORE_RIGHTS);
-        SimpleEntry<String, String> keyAliasEntry = new SimpleEntry<>(Options.ISSUER_KEY_ALIAS, Options.ISSUER_KEY_RIGHTS);
+        SimpleEntry<String, String> keyStoreEntry = new SimpleEntry<>(Options.KEY_STORE_FILE,
+                                                                      Options.KEY_STORE_RIGHTS);
+        SimpleEntry<String, String> keyAliasEntry = new SimpleEntry<>(Options.ISSUER_KEY_ALIAS,
+                                                                      Options.ISSUER_KEY_RIGHTS);
         updateKeyAliasPassword(params, keyStoreEntry, keyAliasEntry, PROMPT_ISSUER_KEY_PWD);
     }
 
+    /**
+     * Updates the password parameters for issuer key alias from issuer keystore.
+     * 
+     * @param params The options object containing various parameters.
+     */
     public static void updateParamForIssuerKeyPwd(Options params) {
-        SimpleEntry<String, String> keyStoreEntry = new SimpleEntry<>(Options.ISSUER_KEY_STORE_FILE, Options.ISSUER_KEY_STORE_RIGHTS);
-        SimpleEntry<String, String> keyAliasEntry = new SimpleEntry<>(Options.ISSUER_KEY_ALIAS, Options.ISSUER_KEY_RIGHTS);
+        SimpleEntry<String, String> keyStoreEntry = new SimpleEntry<>(Options.ISSUER_KEY_STORE_FILE,
+                                                                      Options.ISSUER_KEY_STORE_RIGHTS);
+        SimpleEntry<String, String> keyAliasEntry = new SimpleEntry<>(Options.ISSUER_KEY_ALIAS,
+                                                                      Options.ISSUER_KEY_RIGHTS);
         updateKeyAliasPassword(params, keyStoreEntry, keyAliasEntry, PROMPT_ISSUER_KEY_PWD);
     }
 
+    /**
+     * Updates the password parameters for keystore.
+     * 
+     * @param params The options object containing various parameters.
+     */
     public static void updateParamForKeystorePwd(Options params) {
         updateKeystorePassword(
                 params,
@@ -148,6 +227,11 @@ public class EnterPassword {
         );
     }
 
+    /**
+     * Updates the password parameters for issuer keystore.
+     * 
+     * @param params The options object containing various parameters.
+     */
     public static void updateParamForIssuerKeystorePwd(Options params) {
         updateKeystorePassword(
                 params,
